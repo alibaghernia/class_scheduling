b9bb3b52cf971b434af1d92db69742bc65ebf10e
Ali Baghernia <alibaghernia0@gmail.com>
2025-12-18 00:56:10 +0330

    feat: add requisites relation to feat: add Course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Course extends Model
 {
@@ -14,5 +15,46 @@ class Course extends Model
     public function semester(): BelongsTo
     {
         return $this->belongsTo(Semester::class);
+    }
+
+    public function prerequisites(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Course::class,
+            'course_prerequisites',
+            'course_id',               // کلید این درس
+            'prerequisite_course_id'   // کلید پیش‌نیاز
+        );
+    }
+
+    public function prerequisiteFor(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Course::class,
+            'course_prerequisites',
+            'prerequisite_course_id',  // این درس به عنوان پیش‌نیاز
+            'course_id'                // درسی که به آن وابسته است
+        );
+    }
+
+
+    public function corequisites(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Course::class,
+            'course_corequisites',
+            'course_id',               // کلید این درس
+            'corequisite_course_id'   // کلید پیش‌نیاز
+        );
+    }
+
+    public function corequisiteFor(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Course::class,
+            'course_corequisites',
+            'corequisite_course_id',  // این درس به عنوان پیش‌نیاز
+            'course_id'                // درسی که به آن وابسته است
+        );
     }
 }
